493d66eb6633553b3d581402b979829daba5f0ce
Erin <eringlabe@gmail.com>
2017-04-25 12:51:20 -0700

    sql schema

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+### Schema
+
+CREATE DATABASE `burgers_db`;
+
+USE `burgers_db`;
+
+CREATE TABLE `burgers` 
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    `burger_name` VARCHAR(255) NOT NULL,
+    `devoured` BOOLEAN DEFAULT false,
+    `date` DATETIME,
+    PRIMARY KEY ('id')
+);
+

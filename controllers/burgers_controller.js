98058776a61076d33b4e4467a344ef7726cfcf14
Erin <eringlabe@gmail.com>
2017-04-26 17:24:20 -0700

    var router

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,7 @@
 var express = require("express");
 
 // require burger.js
+var burger = require("../models/burger.js");
 
 // Create the router for the app, and export the router at the end of your file
 

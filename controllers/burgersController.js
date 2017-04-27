02adfffdb0e0c9058a12375d6d5cd562716187df
Erin <eringlabe@gmail.com>
2017-04-26 17:47:16 -0700

    rename

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,7 +1,15 @@
 var express = require("express");
 
+var router = express.Router();
+
 // require burger.js
 var burger = require("../models/burger.js");
 
 // Create the router for the app, and export the router at the end of your file
 
+router.get("/", function(req, res){
+    res.end();
+});
+
+// Export routes for server.js to use.
+module.exports = router;

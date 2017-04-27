f91084841f9d9f424f79a01ef3ced45735c99d61
Erin <eringlabe@gmail.com>
2017-04-26 17:49:41 -0700

    rename

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Links with burgers controller file
-var routes = require("./controllers/burgers_controller.js");
+var routes = require("./controllers/burgersController.js");
 
 // ?
 app.use("/", routes);

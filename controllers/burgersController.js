57daf550581f84be142a578e07fc98f9ee3095be
Erin <eringlabe@gmail.com>
2017-04-27 18:10:54 -0700

    burger controller select all

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -8,7 +8,13 @@ var burger = require("../models/burger.js");
 // Create the router for the app, and export the router at the end of your file
 
 router.get("/", function(req, res){
-    res.end();
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
 });
 
 // Export routes for server.js to use.

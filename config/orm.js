8301dafd8c45856ac482aa8d3974b4b806ec74a0
Erin <eringlabe@gmail.com>
2017-04-27 17:51:07 -0700

    ORM

--- config/orm.js
+++ config/orm.js
@@ -2,18 +2,35 @@
 var connection = require("./connection.js");
 
 var orm = {
-    selectAll: function() {
-
+    selectAll: function(cb) {
+        var queryString = "SELECT * FROM burgers";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
     },
 
-    insertOne: function() {
-
+    insertOne: function(burger, cb) {
+        var queryString = "INSERT INTO burger (`burger_name`) VALUE (" + burger + ")";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        })
     },
 
-    updateOne: function() {
-
+    updateOne: function(id, cb) {
+        var queryString = "UPDATE burgers SET `devoured` = true WHERE id = " + id;
+        connection.query(queryString, function(err, result){
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        })
     }
-
 };
 
 module.exports = orm;

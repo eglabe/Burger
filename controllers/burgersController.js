b88ddc1619079af36d5f4d6a8d2e498e2d002717
Erin <eringlabe@gmail.com>
2017-04-28 10:01:41 -0700

    controller post

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -7,13 +7,19 @@ var burger = require("../models/burger.js");
 
 // Create the router for the app, and export the router at the end of your file
 
-router.get("/", function(req, res){
+router.get("/", function(req, res) {
     burger.selectAll(function(data) {
         var hbsObject = {
             burgers: data
         };
         console.log(hbsObject);
         res.render("index", hbsObject);
+    });
+});
+
+router.post("/", function(req, res) {
+    burger.insertOne(req.body.burgerName, function() {
+        res.redirect("/");
     });
 });
 
